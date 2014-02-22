8c88f43d5cd00bc97485d776fee347ee934678d7
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2014-02-22 14:07:10 +0000

    barebones rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BudgieBackend::Application.config.secret_key_base = 'f76a19a3aa2ff09c01fc4a1d67c9e0673698ada1f960b774c03d8d9174de5a163e2cf87c7423f00035dc52e277584458ebb487591d1778049fc0460ce4c90f4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BudgieBackend::Application.config.session_store :cookie_store, key: '_Budgie_backend_session'
